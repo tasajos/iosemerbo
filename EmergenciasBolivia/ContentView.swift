8f52c4de40972aa03c9ee1d1830ffc880f13b3d4
Carlos Azcarraga <tasajos@gmail.com>
2024-08-18 08:39:07 -0400

    HomeScreen Done

--- EmergenciasBolivia/ContentView.swift
+++ EmergenciasBolivia/ContentView.swift
@@ -10,50 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            // Título
-            Text("Emergencias de Bolivia")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundColor(.white)
-                .padding(.top, 50)
-
-            // Imagen
-            Image("voluntario")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 150, height: 150)
-                .padding(.top, 30)
-                .foregroundColor(.white)
-
-            // Subtítulo
-            Text("Conoce, Apoya y Reporta a las unidades de Emergencia de Bolivia")
-                .font(.subheadline)
-                .foregroundColor(.white)
-                .padding(.top, 20)
-                .multilineTextAlignment(.center)
-                .padding(.horizontal, 40)
-
-            Spacer()
-
-            // Botón de Ingresar
-            Button(action: {
-                print("Botón INGRESAR presionado")
-                // Aquí puedes manejar la navegación
-            }) {
-                Text("INGRESAR")
-                    .font(.title2)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                    .padding(.horizontal, 50)
-            }
-            .padding(.bottom, 50)
-        }
-        .background(Color(red: 0.0, green: 0.74, blue: 0.84))
-        .edgesIgnoringSafeArea(.all)
+        HomeScreen() // Aquí integras la vista HomeScreen en ContentView
     }
 }
 
@@ -62,3 +19,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
